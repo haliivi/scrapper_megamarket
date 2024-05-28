cfe8bd5bad08f2abfd68288d8755aa89315c4071
= <haliivi@mail.ru>
2024-05-28 13:05:35 +0700

    Add input amount of page for parsing.

--- main.py
+++ main.py
@@ -14,19 +14,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 BASEURL = 'https://megamarket.ru'
 
 
-def get_pages_html(url):
+def get_pages_html(url, parsing_page=1):
     options = ChromeOptions()
     options.add_argument('--width=1200')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     items = []
     try:
-        for page in range(1, 3):
+        for page in range(1, parsing_page + 1):
             print(f'[+] Страница {page}')
-            driver.get(url=url.replace(f'page_num', f'page-{page}'))
+            current_url_page = url.replace(f'page_num', f'page-{page}')
+            driver.get(url=current_url_page)
             WebDriverWait(driver, 60).until(
                 ec.presence_of_element_located((By.CLASS_NAME, 'catalog-items-list'))
             )
-            if not get_items(driver.page_source, items):
+            if not get_items(driver.page_source, items, current_url_page):
                 break
     except Exception as e:
         print(e)
@@ -36,7 +37,7 @@ def get_pages_html(url):
     return items
 
 
-def get_items(html, items):
+def get_items(html, items, current_url_page):
     soup = BeautifulSoup(html, 'html.parser')
     items_divs = soup.find('div', class_='catalog-items-list').find_all('div', recursive=False)
     if len(items_divs) == 0:
@@ -59,6 +60,7 @@ def get_items(html, items):
             'Цена': price,
             'Сумма бонусов': bonus,
             'Процент бонуса': bonus_percent,
+            'Ссылка на страницу с товарами': current_url_page,
             'Ссылка на товар': link,
         })
     return True
@@ -73,9 +75,11 @@ def save_to_excel(data: list, filename: str):
 
 def start():
     target = input('Введите название товара: ')
-    min_price = input('Минимальная цена (enter, чтобы пропустить): ')
+    parsing_page = int(parsing_page_input) if (
+        (parsing_page_input := input('Введите количество страниц для парсинга (enter, 1): ')).isdigit()) else 1
+    min_price = input('Минимальная цена (enter, 0): ')
     min_price = min_price if min_price != '' else '0'
-    max_price = input('Максимальная цена (enter, чтобы пропустить): ')
+    max_price = input('Максимальная цена (enter, 9999999): ')
     max_price = max_price if max_price != '' else '9999999'
     target_url = f"{BASEURL}/catalog/page_num/?q={target}"
     if all([max_price, min_price, max_price.isdigit(), min_price.isdigit()]):
@@ -86,7 +90,7 @@ def start():
         json_data = json.dumps(filter)
         url_encoded_data = parse.quote(json_data)
         target_url += '#?filters=' + url_encoded_data
-    items = get_pages_html(target_url)
+    items = get_pages_html(target_url, parsing_page)
     save_to_excel(items, target)
 
 
